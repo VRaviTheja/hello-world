574adeb5c2abf42fe192b3d304d9b932260366d0
Venuthurla Venkata Ravi Theja Reddy <ravithejareddy99@gmail.com>
2017-02-21 18:21:14 +0530

    Create stack_exp.cpp
    Expression parsing using stack

---
+++ stack_exp.cpp
@@ -0,0 +1,53 @@
+#include <map>
+#include <set>
+#include <list>
+#include <cmath>
+#include <ctime>
+#include <deque>
+#include <queue>
+#include <stack>
+#include <string>
+#include <bitset>
+#include <cstdio>
+#include <limits>
+#include <vector>
+#include <climits>
+#include <cstring>
+#include <cstdlib>
+#include <fstream>
+#include <numeric>
+#include <sstream>
+#include <iostream>
+#include <algorithm>
+#include <unordered_map>
+
+using namespace std;
+class Node{
+    char c;
+    Node *next;
+    Node(char c){
+        this->c = c;
+    }
+};
+class stack{
+    Node *top;
+    int size;
+    
+}s;
+bool is_balanced(string expression) {
+    
+}
+
+int main(){
+    int t;
+    cin >> t;
+    for(int a0 = 0; a0 < t; a0++){
+        string expression;
+        cin >> expression;
+        bool answer = is_balanced(expression);
+        if(answer)
+            cout << "YES\n";
+        else cout << "NO\n";
+    }
+    return 0;
+}
